3df6c65bd56ec948d9d4115130fbbb9ebf349756
NathanNeelis <nathan.neelis@hva.nl>
2020-06-11 14:28:01 +0200

    Fixed data connection homepage. Also added filters

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,49 @@
-const index = (req, res) => {
-    // if (!req.session.sessionID) {
-	// 	res.redirect('/login');
-	// } else {
-	// 	res.redirect('/profile');
-    // }
-    console.log(req.session.id);
-    res.render('index');
+// /* eslint-disable */
+
+const User = require('../user');
+
+const index = async (req, res, next) => {
+  const allData = await User.find();
+  const dataBG = await User.find({
+    interests: 'board games' // Looks in all data for people that have Board games in their interests
+  });
+  const dataComics = await User.find({
+    interests: 'comics' // Looks in all data for people that have comics in their interests
+  });
+
+  if (req.session.sessionID) {
+    // Is there a user logged in? If so then:
+    const myData = await User.findOne({
+      _id: req.session.sessionID,
+    });
+
+    if (!allData) {
+      res.send('Error occured while retrieving data');
+    }
+
+    const done = (allData, myData, dataBG, dataComics) => {
+      console.log('this is comics data', dataComics);
+      res.render('index.ejs', {
+        user: myData,
+        data: allData,
+        dataBG: dataBG,
+        dataComics: dataComics,
+      });
+    };
+    done(allData, myData, dataBG, dataComics);
+
+
+  } else if (!req.session.sessionID) { // If there is no user logged in:
+    const done = (allData, dataBG, dataComics) => {
+      res.render('index.ejs', {
+        user: req.session.user,
+        data: allData,
+        dataBG: dataBG,
+        dataComics: dataComics,
+      });
+    };
+    done(allData, dataBG, dataComics);
+  }
 };
 
 module.exports = index;
