8959b0aaca9221ebad55ef25508b9aa01a6649ac
NathanNeelis <nathan.neelis@hva.nl>
2020-06-11 15:51:34 +0200

    Fixed the search page data connection

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-const 
+const
     router = require('express').Router(),
     upload = require('./multer'),
     registerpost = require('./routes/registerpost'),
@@ -7,21 +7,22 @@ const
     loginpost = require('./routes/loginpost'),
     logout = require('./routes/logout'),
     profile = require('./routes/profile'),
-    login = require('./routes/login');
+    login = require('./routes/login'),
+    search = require('./routes/search');
 
 const userRedirectLogin = (req, res, next) => {
-	if (!req.session.sessionID) {
-		res.redirect('/login');
-	} else {
-		next();
+    if (!req.session.sessionID) {
+        res.redirect('/login');
+    } else {
+        next();
     }
 };
 
 const userRedirectProfile = (req, res, next) => {
-	if (req.session.sessionID) {
-		res.redirect('/profile');
-	} else {
-		next();
+    if (req.session.sessionID) {
+        res.redirect('/profile');
+    } else {
+        next();
     }
 };
 
@@ -31,6 +32,7 @@ router
     .get('/register', userRedirectProfile, register)
     .get('/logout', userRedirectLogin, logout)
     .get('/profile', userRedirectLogin, profile)
+    .get('/search', search)
     .post('/login', loginpost)
     .post('/register', upload.single('signupAvatar'), registerpost);
 
