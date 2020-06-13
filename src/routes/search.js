ba354b8af51ea6e109e5b84e2bceae20124b0888
NathanNeelis <nathan.neelis@hva.nl>
2020-06-13 13:14:01 +0200

    added some comments

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,42 +1,38 @@
 const User = require('../user');
 
-
 const search = async (req, res) => {
     try {
-        const allData = await User.find();
-        const user = await User.findOne({
+        const allData = await User.find(); // all data in database
+        const user = await User.findOne({ // find session user
             _id: req.session.sessionID,
         });
-        const search = await User.find({
+        const search = await User.find({ // find data that equals filtered activity
             interests: req.body.activity
         });
-        const activity = req.body.activity;
+        const activity = req.body.activity; // filtered activity
 
         const done = (allData, user, search, activity) => {
-            req.session.search = search;
-            if (user) {
-                // console.log(req.session.search);
+            req.session.search = search; // makes a session on the filtered activity
+            if (user) { // if there is a user logged in render the search page with he following data
                 req.session.sessionID = user._id;
                 res.render('search.ejs', {
-                    dataFilter: req.session.search,
                     data: allData,
                     user: user,
+                    dataFilter: req.session.search,
                     activity: activity
                 });
-            } else {
-
+            } else { // if there is not a user logged in render the search page with this data
                 res.render('search.ejs', {
                     data: allData,
                     dataFilter: req.session.search,
                     activity: activity
                 });
-                console.log(activity);
             }
         };
         done(allData, user, search, activity);
     }
     catch (err) {
-        res.send('something went wrong in the gathering the data');
+        res.send('something went wrong in the gathering the data'); // Error handling
     }
 };
 
