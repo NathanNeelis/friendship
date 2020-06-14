8079ae7b0c5ad527b08bfc028c82c85bb7fa953b
NathanNeelis <nathan.neelis@hva.nl>
2020-06-14 14:45:18 +0200

    Some more tries on filtering

--- static/js/filter.js
+++ static/js/filter.js
@@ -6,10 +6,34 @@ request.send();
 
 request.onload = function () {
     const allUsers = request.response;
-    log(allUsers);
+    setHeader();
+    showUsers(allUsers);
 };
 
 
-const log = (jsonObj) => {
-    console.log(jsonObj);
-};
+function setHeader() {
+    let myH2 = document.getElementById('myH2');
+    myH2.textContent = 'These people might be interesting to you!';
+
+}
+
+function showUsers(jsonObj) {
+    const users = jsonObj;
+    console.log(users);
+
+    for (let i = 0; i < users.length; i++) {
+        console.log(users[i]);
+        // const myArticle = document.createElement('article');
+        const myH4 = document.getElementById('myH4');
+        const myPara1 = document.getElementById('myPara1');
+
+        myH4.textContent = users[i].firstname;
+        myPara1.textContent = users[i].location;
+
+    }
+}
+
+
+// Resources
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/JSON
+// https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild
