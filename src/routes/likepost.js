369ba5c92a35ee30e999570bf3ebc6ce6308af27
Bjornborgie <bjorn.kouw2@gmail.com>
2020-06-15 17:33:16 +0200

    debugging the unmatch

--- src/routes/likepost.js
+++ src/routes/likepost.js
@@ -17,7 +17,8 @@ const likepost = (req, res) => {
         }
     });
     // Hier zoek je op de user die je hebt geliked (req.body.like), en check je of deze user jou ook heeft geliked. Als dat zo is kan je dat direct renderen op een pagina.
-    const checkIfUserLiked = () => {
+    const checkIfUserLiked = (x) => {
+        console.log(x);
         User.findOne({
             _id: req.body.like
         }, (err, user) => {
@@ -25,10 +26,22 @@ const likepost = (req, res) => {
                 console.log('MongoDB Error:' + err);
             }
             if (user) {
-                if (user.Likes.includes(String(req.session.sessionID))) {
-console.log("match")
-                } else {
-                    console.log("nomatch")
+                console.log(x.Likes.includes(String(req.body.like)));
+                console.log(user.Likes.includes(String(req.session.sessionID)));
+
+                if (user.Likes.includes(String(req.session.sessionID) == true) && x.Likes.includes(String(req.body.like)) == true) {
+                    console.log("unmatch")
+                    Unmatch()
+                    
+                }
+                else if (user.Likes.includes(String(req.session.sessionID)== true && x.Likes.includes(String(req.body.like))) == false) {
+                    console.log("match");
+
+                } 
+                
+
+                else {
+                    console.log("nomatch");
 
                 }
         //     } else {
@@ -51,12 +64,52 @@ console.log("match")
                 if (err) {
                     console.log(err);
                 } else {
-                   checkIfUserLiked();
+
+                    User.findOne({
+                        _id: req.session.sessionID
+                    }, (err, user) => {
+                        if (err) {
+                            console.log('MongoDB Error:' + err);
+                        }
+                        else {
+                            checkIfUserLiked(user);
+         
+                         }
+                    })
+       
                 }
             }
         );
     };
 }
+
+
+
+
+
+
+const Unmatch = () => {
+    console.log("unmatch, started")
+
+    User.updateOne({
+            _id: req.body.like
+        }, {
+            $pull: {
+                Likes: String(req.session.sessionID)
+            }
+        },
+        (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                
+
+            }
+        });
+                };
+   
+            
+            
 
 
 
