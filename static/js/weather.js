0ad2b73c1c27005deea049c6d502833f82828669
NathanNeelis <nathan.neelis@hva.nl>
2020-06-19 14:07:53 +0200

    Fixed temp switch

--- static/js/weather.js
+++ static/js/weather.js
@@ -9,7 +9,7 @@ async function catchWeather(req, res) {
             const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userLocation + ',nl&units=metric&appid=7cb76bd2c75726e5aa77abb6c6de9b09')
             const weatherData = await response.json();
             fetchApiMore(weatherData);
-            if (weatherData.main.temp > 38) {
+            if (weatherData.main.temp > 19) {
                 mtb(weatherData);
                 // loading mountainbike profiles from database
                 mtbSection.classList.add('games-hide');
