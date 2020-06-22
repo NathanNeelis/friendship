eaa1c25f24229b2e16a159d165c18be96dc2c197
NathanNeelis <nathan.neelis@hva.nl>
2020-06-22 18:29:53 +0200

    filter results without your own profile and matches

--- src/routes/search.js
+++ src/routes/search.js
@@ -3,29 +3,46 @@ const User = require('../user');
 const search = async (req, res) => {
     try {
         const allData = await User.find(); // all data in database
-        const user = await User.findOne({ // find session user
+        const myData = await User.findOne({ // find session user
             _id: req.session.sessionID,
         });
         const search = await User.find({ // find data that equals filtered activity
+            _id: { $nin: req.session.sessionID },
             interests: req.body.activity
         });
         const activity = req.body.activity; // filtered activity
 
-        if (user) { // checks if there is a user logged in
-            const done = async (allData, user, search, activity) => {
-                const equalActivities = await User.find({ // finds other users with the same interests 
-                    interests: user.interests[0] // How to create loop here?
+        if (req.session.sessionID) { // checks if there is a user logged in
+            let filteredData = (dataToFilter) => {
+                dataToFilter.forEach((result) => {
+                    if (result.likes.includes(myData._id) && myData.likes.includes(result._id)) {
+                        let cleantheArray = dataToFilter.indexOf(result);
+                        dataToFilter.splice(cleantheArray, 1);
+                    }
                 });
+
+                return dataToFilter;
+            };
+
+            const done = async (allData, myData, search, activity) => {
+                const equalActivities = await User.find({ // finds other users with the same interests 
+                    _id: { $nin: req.session.sessionID },
+                    interests: myData.interests[0] // How to create loop here?
+                });
+                let equalActivitiesFiltered = filteredData(equalActivities);
+                let allDataFiltered = filteredData(allData);
+                let searchFiltered = filteredData(search);
+
                 req.session.search = search; // makes a session on the filtered activity
                 res.render('search', {
-                    data: allData,
-                    user: user,
-                    dataFilter: req.session.search,
+                    data: allDataFiltered,
+                    user: myData,
+                    dataFilter: searchFiltered,
                     activity: activity,
-                    dataEqual: equalActivities
+                    dataEqual: equalActivitiesFiltered
                 });
             };
-            done(allData, user, search, activity);
+            done(allData, myData, search, activity);
         } else { // if there is no user logged in
             const done = async (allData, search, activity) => {
                 req.session.search = search;
